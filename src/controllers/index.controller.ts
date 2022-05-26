ff67ce4fbd693dac3218caeca77b2652fb59e44a
Brian Angulo <bangulo219@gmail.com>
2022-05-26 00:26:12 -0500

    feat: scaffolding with a memes api

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from 'routing-controllers';
-
+import axios from 'axios';
 @Controller()
 export class IndexController {
-  @Get('/')
-  index() {
-    return 'OK';
+  @Get('/api/memes')
+  async index() {
+    const endpoint = 'https://api.imgflip.com/get_memes';
+    const memesResponse = await axios.get(endpoint);
+    const memes = memesResponse.data.data ? memesResponse.data.data : memesResponse.data;
+    return memes;
   }
 }
